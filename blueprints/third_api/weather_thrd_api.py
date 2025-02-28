1ff76d131aebc7267fe56ecb8c6b5adeca077338
locke <lockeglassdev@gmail.com>
2025-02-28 06:58:15 -0600

    forecast weather format function was created for get only the required data

--- blueprints/third_api/weather_thrd_api.py
+++ blueprints/third_api/weather_thrd_api.py
@@ -1,31 +1,57 @@
 import requests
-import os
+from typing import Tuple,List
 
 WEATHER_API_KEY = "819712d29f4b4071bb702425252702"
 WEATHER_URL_BASE = "https://api.weatherapi.com/v1"
 CURRENT_DAY_TOPIC = "/current.json"
 FORECAST_DAYS_TOPIC = "/forecast.json"
 
-def current_weather_api(location:str,lang:str,days:int=1):
+def current_weather_api(location:str,lang:str,days:int=1) -> dict | None:
+    """
+    This function provides a conection to WeatherAPI, for get
+    the forecast weather in the next days.
+
+    :param location: The location weather to get
+    :type location: str
+    :param lang: The language of the response
+    :type lang: str
+    :param days: The next 'n' forecast weather days, defaults to 1
+    :type days: int, optional
+    :return: A dictionary with JSON data response if exists else None.
+    :rtype: dict | None
+    """
     result = None
+
+    # The base query params for WeatherAPI
     query_data = {
         "q":location,
         "lang":lang,
         "key":WEATHER_API_KEY
     }
 
-    if days == 1:
-        query = WEATHER_URL_BASE + CURRENT_DAY_TOPIC
-        result = requests.get(query,params=query_data)
-    else:
-        query = WEATHER_URL_BASE + FORECAST_DAYS_TOPIC
-        query_data["days"] = days
-        result = requests.get(query,params=query_data)
+    query = WEATHER_URL_BASE + FORECAST_DAYS_TOPIC
+    query_data["days"] = days
+    result = requests.get(query,params=query_data)
 
     if result.status_code == 200:
         return result.json()
     
     return None
 
+def forecast_exctract_data(data:dict) -> Tuple[dict,List[dict]]:
+    """
+    This function extracts only the required data to make
+    a JSON response from the app clients.
+
+    :param data: A JSON data response as a dict
+    :type data: dict
+    :return: The location info and the forecast of next days.
+    :rtype: Tuple[dict,List[dict]]
+    """
+    location_info = data["location"]
+    forecast = data["forecast"]["forecastday"]
+
+    return location_info,forecast
+
 if __name__ == "__main__":
-    print(current_weather_api("Mexico","es",2))
+    pass
